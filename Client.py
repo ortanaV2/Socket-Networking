da2fd71822ad002cdafa2f20bce0afb6f5eb6e67
Ortana <126905156+ortanaV2@users.noreply.github.com>
2023-12-10 17:21:37 +0100

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,25 @@
+"""Example-Code for a client connecting and comunicating with the server"""
+
+import socket
+
+host = '192.123.4.567' #Servers IP-Address
+port = 10883
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((host, port))
+print(f"Build connection to server --> {host}:{port}")
+
+while True:
+    message = input("Server request: ")
+    if message == "$kill": break
+    client_socket.send(message.encode('utf-8'))
+
+    data = client_socket.recv(1024)    
+    if not data:
+        print("Server response-status: 500")
+        break
+    else:
+        print(f"Server response-data: {data.decode('utf-8')}")
+
+client_socket.close()
