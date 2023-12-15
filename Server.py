20e4f0494e3612be058a039e72ae4b7fee9898ee
Ortana <126905156+ortanaV2@users.noreply.github.com>
2023-12-15 08:12:38 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -8,11 +8,9 @@ with open(r"./config.json", "r") as config_f:
 def handle_client(client_socket, client_address):
     while True:
         data = client_socket.recv(1024)
-        if not data:
-            break
-        
+        if not data: break
         decoded_message = data.decode('utf-8')
-        print(f"Received data from {client_address}: {decoded_message}")
+        print(f"{client_address} >>: {decoded_message}")
 
         try:
             response = CONFIG[str(decoded_message)]
@@ -20,35 +18,31 @@ def handle_client(client_socket, client_address):
             response = 404
         except Exception:
             response = 500
-            
-        print("response: {0}".format(response))
+        print(f"Response: {response}")
+        
         client_socket.send(str(response).encode('utf-8'))
-
     client_socket.close()
+
 def start_server(port):
     host = '0.0.0.0'
-
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen()
-    print(f"Server listening at --> {host}:{port}")
+    print(f"Server listening --> {host}:{port}")
 
     while True:
         client_socket, client_address = server_socket.accept()
-        print(f"Connection-build accepted --> {client_address}")
-
+        print(f"Accepted --> {client_address}")
         client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_handler.start()
 
 if __name__ == "__main__":
     port1 = 10883
     port2 = 10884
-
     server_thread1 = threading.Thread(target=start_server, args=(port1,))
     server_thread2 = threading.Thread(target=start_server, args=(port2,))
 
     server_thread1.start()
     server_thread2.start()
-
     server_thread1.join()
     server_thread2.join()
